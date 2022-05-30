9ca6a221a8179329127dc2c849541ef765f11b80
Kenyan Burnham <33104632+KenyanBurnham@users.noreply.github.com>
2022-05-30 12:41:53 -0500

    Updating the scores for round 2

--- bracket.js
+++ bracket.js
@@ -3,74 +3,133 @@ Instructions:
 1. Download the totally valid names file as a CSV
 2. Go to https://www.convertcsv.com/csv-to-json.htm
 3. Input the CSV file and ask for "CTV to JSON array"
+
+let nameArray = [
+  ["Vanilla Stout Porter",5],
+  ["Brief",1],
+  ["Samuel \"Bridges\" Porter",2],
+  ["Martin House Porter",8],
+  ["Cole Wehrle Porter",4],
+  ["Ben N. Syder Porter",6],
+  ["Kareem O'Weet Porter",2],
+  ["Jed I. Knight Porter",4],
+  ["Uwe Eurotrash Porter *sponsored by BGGCon*",13 ],
+  ["Wayne Kerr Porter",0],
+  ["Mae Denless Porter",1],
+  ["Tylorine Carler Porter",8],
+  ["Marina Quey Porter",2],
+  ["Steve Irwin Porter",4],
+  ["Taylor tha Creator Porter",3],
+  ["Ashcan Porter",7],
+  ["P.P. Hertz Porter",5],
+  ["Gaunter O'Dimm Porter",1],
+  ["Alotta Fagina Porter",2],
+  ["Kenya Swallow Porter",8],
+  ["Yuri Nate Porter",4],
+  ["Charity Case Porter",6],
+  ["Yuri Dopted Porter",8],
+  ["Nugget",2],
+  ["Formula Juan Porter",6],
+  ["Port au Prince Porter",0],
+  ["Baliin King Porter",7],
+  ["Dwalin Bee Porter",3],
+  ["Fudgecicle Mocha Porter",2],
+  ["Guisseppe Tuscany Porter",4],
+  ["Mae Patty Baldwin Ness Porter",3],
+  ["Hey Porter",7],
+  ["Harry Porter",9],
+  ["Rafael Edward \"Ted\" Cruz Porter",2],
+  ["Beto O Porter",10],
+  ["Moe Lester Porter",1],
+  ["Taks Credditte Porter",12],
+  ["Brick \"Andy\" Mortar Porter",2],
+  ["Brooke N. Rubbers Porter",6],
+  ["Ivana Humpalotte Porter",2],
+  ["Warner Horner McWhorter Porter",4],
+  ["Monday September Nineteenth Porter",2],
+  ["La-a (\"ladasha\") Porter",3],
+  ["John Jacob Jingleheimer Schmidt Porter",7],
+  ["Crystal Tits Porter",4],
+  ["Ellis D. Porter",2],
+  ["Little Sweetmeat Porter",6],
+  ["Eaton Dariche Porter",4],
+  ["Baby McBaby Face Porter",2],
+  ["Bubba Porter",9],
+  ["Boar Porter ",2],
+  ["Porterhouse Steak Porter",9],
+  ["Portland Patagonia Porter ",4],
+  ["Torpor Porter",6],
+  ["Twoler Porter",6],
+  ["Papa Porter ",0],
+  ["Eunuch Porter ",0],
+  ["PeeWee Porter",11],
+  ["P. Sherman, 42 Wallaby Way, Sydney, Porter ",2],
+  ["Joseph Ray Porter",9],
+  ["Brownie Shytles Porter",1],
+  ["Johnny Sins Porter",9],
+  ["Lil' Sweet Porter",2],
+  ["Marijauna Jesus Porter",10]
+];
+
 **/
+
 //Use this link to generate a
     let nameArray = [
       ["Vanilla Stout Porter",5],
-      ["Brief",1],
-      ["Samuel \"Bridges\" Porter",3],
-      ["Martin House Porter",3],
-      ["Cole Wehrle Porter",3],
-      ["Ben N. Syder Porter",3],
-      ["Kareem O'Weet Porter",2],
+      ["Martin House Porter",8],
+      ["Ben N. Syder Porter",6],
       ["Jed I. Knight Porter",4],
-      ["Uwe Eurotrash Porter *sponsored by BGGCon*",6 ],
-      ["Wayne Kerr Porter",0],
-      ["Mae Denless Porter",1],
-      ["Tylorine Carler Porter",5],
-      ["Marina Quey Porter",2],
+      ["Uwe Eurotrash Porter *sponsored by BGGCon*",13 ],
+      ["Tylorine Carler Porter",8],
       ["Steve Irwin Porter",4],
-      ["Taylor tha Creator Porter",3],
-      ["Ashcan Porter",3],
+      ["Ashcan Porter",7],
       ["P.P. Hertz Porter",5],
-      ["Gaunter O'Dimm Porter",1],
-      ["Alotta Fagina Porter",3],
-      ["Kenya Swallow Porter",3],
-      ["Yuri Nate Porter",3],
-      ["Charity Case Porter",3],
-      ["Yuri Dopted Porter",3],
-      ["Nugget",3],
+      ["Kenya Swallow Porter",8],
+      ["Charity Case Porter",6],
+      ["Yuri Dopted Porter",8],
       ["Formula Juan Porter",6],
-      ["Port au Prince Porter",0],
-      ["Baliin King Porter",3],
-      ["Dwalin Bee Porter",3],
-      ["Fudgecicle Mocha Porter",2],
+      ["Baliin King Porter",7],
       ["Guisseppe Tuscany Porter",4],
-      ["Mae Patty Baldwin Ness Porter",3],
-      ["Hey Porter",3],
-      ["Harry Porter",4],
-      ["Rafael Edward \"Ted\" Cruz Porter",2],
-      ["Beto O Porter",5],
-      ["Moe Lester Porter",1],
-      ["Taks Credditte Porter",4],
-      ["Brick \"Andy\" Mortar Porter",2],
-      ["Brooke N. Rubbers Porter",4],
-      ["Ivana Humpalotte Porter",2],
+      ["Hey Porter",7],
+      ["Harry Porter",9],
+      ["Beto O Porter",10],
+      ["Taks Credditte Porter",12],
+      ["Brooke N. Rubbers Porter",6],
       ["Warner Horner McWhorter Porter",4],
-      ["Monday September Nineteenth Porter",2],
-      ["La-a (\"ladasha\") Porter",3],
-      ["John Jacob Jingleheimer Schmidt Porter",3],
+      ["John Jacob Jingleheimer Schmidt Porter",7],
       ["Crystal Tits Porter",4],
-      ["Ellis D. Porter",2],
-      ["Little Sweetmeat Porter",3],
-      ["Eaton Dariche Porter",3],
-      ["Baby McBaby Face Porter",2],
-      ["Bubba Porter",4],
-      ["Boar Porter ",2],
-      ["Porterhouse Steak Porter",4],
-      ["Portland Patagonia Porter ",3],
-      ["Torpor Porter",3],
+      ["Little Sweetmeat Porter",6],
+      ["Bubba Porter",9],
+      ["Porterhouse Steak Porter",9],
+      ["Torpor Porter",6],
       ["Twoler Porter",6],
-      ["Papa Porter ",0],
-      ["Eunuch Porter ",0],
-      ["PeeWee Porter",6],
-      ["P. Sherman, 42 Wallaby Way, Sydney, Porter ",2],
-      ["Joseph Ray Porter",4],
-      ["Brownie Shytles Porter",1],
-      ["Johnny Sins Porter",5],
-      ["Lil' Sweet Porter",2],
-      ["Marijauna Jesus Porter",4]
+      ["PeeWee Porter",11],
+      ["Joseph Ray Porter",9],
+      ["Johnny Sins Porter",9],
+      ["Marijauna Jesus Porter",10]
 ];
+
+/**
+Vanilla Stout v. Martin House Porter
+Ben N. Syder v. Jed I. Knight
+Uwe v. Tylorine
+Steve irwin v. Ashcan
+P.P. hertz v. Kenya
+Charity Case v. Yuri Dopted
+Fomula Juan v. Baliin
+Guiseppe v. Hey
+Harry v. Beto
+Taks Credditte v. Brooke
+Warner v. John Jacob
+Crystal tits v. Little Sweetmeat
+Bubba v. Porterhouse
+Torpor v. Twoler
+Peewee v Joseph
+Johnny v. Marijauna
+
+**/
+
+
 
 //============================================
 
